4df02ba12440eecb6cac656102434d99094825b6
Ethan Brews <ethan.brews@hpe.com>
2024-03-12 14:37:44 -0600

    Change config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,8 +17,39 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
 	{ "smoka7/hop.nvim", opts = {  } },
 	{ "nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },
+	{ "ThePrimeagen/harpoon", dependencies = { 'nvim-lua/plenary.nvim' } },
 	{ "tpope/vim-fugitive" },
-	{ "scrooloose/nerdcommenter" }
+	{ "scrooloose/nerdcommenter" },
+	{ "nvim-treesitter/nvim-treesitter" },
+    {
+          "olimorris/onedarkpro.nvim",
+          priority = 1000, -- Ensure it loads first
+    },
+    {
+      "dhananjaylatkar/cscope_maps.nvim",
+      dependencies = {
+        "folke/which-key.nvim", -- optional [for whichkey hints]
+        "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+        "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+        "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
+      },
+      opts = {
+        disable_maps = false, -- "true" disables default keymaps
+        skip_input_prompt = false, -- "true" doesn't ask for input
+        prefix = "<leader>c", -- prefix to trigger maps
+
+        -- cscope related defaults
+        cscope = {
+          exec = "cscope",
+          picker = "telescope",
+          skip_picker_for_single_result = false,
+          project_rooter = {
+            enable = false,
+            change_cwd = false
+          }
+        }
+      }
+    }
 }
 
 -- Basic Settings
@@ -28,12 +59,12 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     command = "setfiletype log"
 })
 
---vim.opt.nocompatible = true
+vim.api.nvim_create_user_command('Vb', 'normal! <C-v>', {})
+
 vim.opt.nu = true
 vim.opt.rnu = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
---vim.opt.syntax = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
@@ -42,10 +73,34 @@ vim.opt.autoindent = true
 vim.opt.ttyfast = true
 vim.opt.wildmode = { 'longest', 'list' }
 vim.opt.listchars = { eol = '$', tab = '>>', trail = '•' }
+vim.fn.matchadd('errorMsg', [[\s\+$]])
 vim.opt.mouse = ""
+vim.opt.termguicolors = true
 
 -- Start plugin manager
 require("lazy").setup(plugins)
+
+
+-- Treesitter Enable
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'cmake', 'markdown', 'bash', 'markdown_inline' }
+}
+
+
+-- Colorscheme
+
+require("onedarkpro").setup({ })
+
+vim.cmd("colorscheme onedark")
+
+-- Keybindings (Cscope)
+
+require("cscope_maps").setup()
 
 
 -- Keybindings (VimFugitive)
@@ -74,10 +129,50 @@ vim.keymap.set('n', 'tp', ':bprevious<CR>')
 vim.keymap.set('n', ';;', ':set list!<CR>', { silent = true })
 vim.keymap.set('n', '<C-l>', ':noh<CR>:let @/ = ""<CR>', { nowait = true, silent = true })
 
+-- Keybindings (Harpoon)
+
+local harpoon = require('harpoon')
+harpoon.setup({
+    global_settings = {
+        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_toggle = false,
+
+        -- saves the harpoon file upon every change. disabling is unrecommended.
+        save_on_change = true,
+
+        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+        enter_on_sendcmd = false,
+
+        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+        tmux_autoclose_windows = false,
+
+        -- filetypes that you want to prevent from adding to the harpoon list menu.
+        excluded_filetypes = { "harpoon" },
+
+        -- set marks specific to each git branch inside git repository
+        mark_branch = false,
+
+        -- enable tabline with harpoon marks
+        tabline = false,
+        tabline_prefix = "   ",
+        tabline_suffix = "   ",
+    }
+})
+
+vim.api.nvim_create_user_command('Rf', function()
+    require("harpoon.mark").add_file()
+end, {})
+
+vim.keymap.set('n', 'tr', function()
+    require("harpoon.ui").toggle_quick_menu()
+end, { silent = true })
+
+
 
 -- Keybindings (Telescope)
 
 local telescope = require('telescope.builtin')
+-- telescope.load_extension('harpoon')
 
 vim.keymap.set('n', 'tl', telescope.buffers)
 vim.keymap.set('n', 'tf', telescope.find_files)
@@ -97,57 +192,3 @@ require "telescope".setup {
     }
   }
 }
-
---[[ telescope.defaults.file_ignore_patterns = {
-    "vendor/*",
-    "%.lock",
-    "__pycache__/*",
-    "%.sqlite3",
-    "%.ipynb",
-    "node_modules/*",
-    "%.jpg",
-    "%.jpeg",
-    "%.png",
-    "%.svg",
-    "%.otf",
-    "%.ttf",
-    ".git/",
-    "%.webp",
-    ".dart_tool/",
-    ".github/",
-    ".gradle/",
-    ".idea/",
-    ".settings/",
-    ".vscode/",
-    "__pycache__/",
-    "build/",
-    "env/",
-    "gradle/",
-    "node_modules/",
-    "target/",
-    "%.pdb",
-    "%.dll",
-    "%.class",
-    "%.exe",
-    "%.cache",
-    "%.ico",
-    "%.pdf",
-    "%.dylib",
-    "%.jar",
-    "%.docx",
-    "%.met",
-    "smalljre_*/*",
-    ".vale/",
-    "%.burp",
-    "%.mp4",
-    "%.mkv",
-    "%.rar",
-    "%.zip",
-    "%.7z",
-    "%.tar",
-    "%.bz2",
-    "%.epub",
-    "%.flac",
-    "%.tar.gz",
-  }
-]]
